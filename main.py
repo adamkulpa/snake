b1ab860f871f67c574f3fc23bb85987591343575
Todevor <todevorek@gmail.com>
2016-07-31 22:15:42 +0200

    Initial commit.

---
+++ main.py
@@ -0,0 +1,22 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+
+'''
+Created on 29 lip 2016
+
+@file: main.py
+@brief: It's small, well-know game realization called 'Snake'.
+@autor: Adam Kulpa
+'''
+
+
+from Application import App
+
+
+def main():
+    app = App()
+    app.on_execute()
+
+
+if __name__ == "__main__" :
+    main()
